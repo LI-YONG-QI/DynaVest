daf9a9391b5c55a9720c991c4b9cd96d96c13291
ckt22 <tangck0202@gmail.com>
2025-05-23 15:12:00 +0800

    add: mock strategy details chart

--- src/app/strategies/[id]/page.tsx
+++ src/app/strategies/[id]/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useParams, notFound } from "next/navigation";
 import { useChat } from "@/contexts/ChatContext";
 import { STRATEGIES_METADATA } from "@/constants/strategies";
+import { StrategyDetailsChart } from "@/components/StrategyList/StrategyDetailsChart";
 
 function getRiskLevelLabel(level: string) {
   switch (level) {
@@ -97,9 +98,10 @@ function StrategyDetailContent() {
                 )}
               </p>
             </div>
+
             <div className="mt-5">
               <div className="bg-white rounded-lg p-1 md:p-2">
-                <div className="grid grid-cols-1 md:grid-cols-5 max-md:divide-y md:divide-x divide-gray-200">
+                <div className="grid grid-cols-1 md:grid-cols-5 max-md:divide-y-2 md:divide-x-2 divide-gray-200">
                   <div className="md:px-4 py-2">
                     <div className="text-xs text-gray-500 uppercase tracking-wider">
                       Protocol
@@ -169,6 +171,17 @@ function StrategyDetailContent() {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <div className="bg-white rounded-lg divide-y-2 divide-gray-200">
+              <div className="p-4">
+                <h2 className="text-2xl font-bold">Charts</h2>
+              </div>
+              <div className="py-4">
+                <StrategyDetailsChart />
               </div>
             </div>
           </div>
