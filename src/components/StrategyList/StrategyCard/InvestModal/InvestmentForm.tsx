4952803a6a5ce2b282bd1e1d4d0323953cb05e03
ckt22 <tangck0202@gmail.com>
2025-05-24 18:43:56 +0800

    add: LP mode for investment form

--- src/components/StrategyList/StrategyCard/InvestModal/InvestmentForm.tsx
+++ src/components/StrategyList/StrategyCard/InvestModal/InvestmentForm.tsx
@@ -7,7 +7,7 @@ import { parseUnits } from "viem";
 
 import useCurrency from "@/hooks/useCurrency";
 import useSwitchChain from "@/hooks/useSwitchChain";
-import { InvestmentFormMode, InvestStrategy } from "@/types";
+import { InvestmentFormMode, InvestStrategy, Token } from "@/types";
 import { MoonLoader } from "react-spinners";
 import { getStrategy } from "@/utils/strategies";
 import { useStrategyExecutor } from "@/hooks/useStrategyExecutor";
@@ -24,6 +24,7 @@ enum ButtonState {
   Pending = "Processing...",
   Invest = "Invest",
   Withdraw = "Withdraw",
+  LP = "Add Liquidity",
   SwitchChain = "Switch Chain",
 }
 
@@ -33,8 +34,27 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
   handleClose,
   handlePortfolio,
 }) => {
+  // first token input
   const [amount, setAmount] = useState<string>("");
   const [showCurrencyDropdown, setShowCurrencyDropdown] = useState(false);
+  const {
+    currency,
+    setCurrency,
+    balance: maxBalance,
+    isLoadingBalance,
+  } = useCurrency(strategy.tokens[0]);
+
+  // second token input - for LP
+  const [secondAmount, setSecondAmount] = useState<string>("");
+  const [showSecondCurrencyDropdown, setShowSecondCurrencyDropdown] =
+    useState(false);
+  const {
+    currency: secondCurrency,
+    setCurrency: setSecondCurrency,
+    balance: secondMaxBalance,
+    isLoadingBalance: isLoadingSecondBalance,
+  } = useCurrency(strategy.tokens?.[1] || strategy.tokens[0]);
+
   const [buttonState, setButtonState] = useState<ButtonState>(
     ButtonState.Pending
   );
@@ -45,12 +65,6 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
     strategy.chainId
   );
   const { switchChainAsync } = useWagmiSwitchChain();
-  const {
-    currency,
-    setCurrency,
-    balance: maxBalance,
-    isLoadingBalance,
-  } = useCurrency(strategy.tokens[0]);
 
   const chainId = useChainId();
   const { execute } = useStrategyExecutor();
@@ -79,6 +93,11 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
   // TODO: Support withdrawal
   const withdraw = () => {
     console.log("Withdraw");
+  };
+
+  // TODO: Support LP
+  const processLp = () => {
+    console.log("LP");
   };
 
   const handleSwitchChain = async (chainId: number) => {
@@ -130,6 +149,9 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
       case ButtonState.Withdraw:
         withdraw();
         break;
+      case ButtonState.LP:
+        processLp();
+        break;
       case ButtonState.SwitchChain:
         handleSwitchChain(strategy.chainId);
         break;
@@ -153,6 +175,8 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
           return ButtonState.Invest;
         case "withdraw":
           return ButtonState.Withdraw;
+        case "lp":
+          return ButtonState.LP;
         default:
           return ButtonState.Pending;
       }
@@ -165,99 +189,37 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
   return (
     <form onSubmit={handleSubmit}>
       {/* Amount input */}
-      <div className="bg-gray-100 rounded-md border border-gray-300 mb-6">
-        <div className="flex items-center w-full gap-2">
-          <input
-            type="text"
-            name="amount"
-            id="amount"
-            className="flex-1 min-w-0 bg-transparent text-gray-500 block px-4 py-3 text-lg font-semibold focus:outline-none focus:ring-0 focus:border-0 placeholder:text-gray-500"
-            placeholder="0.00"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-          {/* Custom dropdown with icons */}
-          <div className="shrink-0 md:min-w-[100px] relative">
-            <button
-              type="button"
-              className="text-sm md:text-lg bg-transparent flex items-center gap-1 md:gap-2 px-2 md:px-4 py-2 font-semibold focus:outline-none rounded-md hover:bg-gray-200"
-              onClick={() => setShowCurrencyDropdown(!showCurrencyDropdown)}
-            >
-              <Image
-                src={currency.icon}
-                alt={currency.name}
-                className="w-6 h-6 object-contain"
-                width={24}
-                height={24}
-              />
-              {currency.name}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-3 w-3 md:h-5 md:w-5 md:ml-1"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-            {showCurrencyDropdown && (
-              <div className="absolute right-0 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10">
-                {strategy.tokens.map((token) => (
-                  <button
-                    key={token.name}
-                    type="button"
-                    className="w-full flex items-center gap-1 px-2 md:gap-2 md:px-4 py-2 text-left hover:bg-gray-100"
-                    onClick={() => {
-                      setCurrency(token);
-                      setShowCurrencyDropdown(false);
-                    }}
-                  >
-                    <Image
-                      src={token.icon}
-                      alt={token.name}
-                      width={24}
-                      height={24}
-                      className="w-6 h-6 object-contain"
-                    />
-                    {token.name}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="flex items-center px-4 pb-2">
-          <div className="flex items-center w-full">
-            <span className="flex items-center gap-2 text-xs md:text-sm text-gray-500">
-              <span>Balance: </span>
-              <div>
-                {isLoadingBalance ? (
-                  <MoonLoader size={10} />
-                ) : isSupportedChain ? (
-                  maxBalance.toFixed(4)
-                ) : (
-                  "NaN"
-                )}
-              </div>
-              <span>{currency.name}</span>
-            </span>
-            <button
-              type="button"
-              onClick={handleSetMax}
-              disabled={!isSupportedChain || isLoadingBalance}
-              className="text-xs md:text-sm font-medium text-[#5F79F1] hover:text-[#4A64DC] focus:outline-none ml-2 border-0 bg-transparent cursor-pointer disabled:opacity-50"
-            >
-              MAX
-            </button>
-          </div>
-        </div>
-      </div>
+      <AmountInput
+        inputName="amount"
+        amount={amount}
+        setAmount={setAmount}
+        currency={currency}
+        setCurrency={setCurrency}
+        showCurrencyDropdown={showCurrencyDropdown}
+        setShowCurrencyDropdown={setShowCurrencyDropdown}
+        strategy={strategy}
+        isLoadingBalance={isLoadingBalance}
+        isSupportedChain={isSupportedChain}
+        maxBalance={maxBalance}
+        handleSetMax={handleSetMax}
+      />
+
+      {mode == "lp" && (
+        <AmountInput
+          inputName="secondAmount"
+          amount={secondAmount}
+          setAmount={setSecondAmount}
+          currency={secondCurrency}
+          setCurrency={setSecondCurrency}
+          showCurrencyDropdown={showSecondCurrencyDropdown}
+          setShowCurrencyDropdown={setShowSecondCurrencyDropdown}
+          strategy={strategy}
+          isLoadingBalance={isLoadingSecondBalance}
+          isSupportedChain={isSupportedChain}
+          maxBalance={secondMaxBalance}
+          handleSetMax={handleSetMax}
+        />
+      )}
 
       {/* Invest button */}
       <button
@@ -268,6 +230,132 @@ const InvestmentForm: FC<InvestmentFormProps> = ({
         {buttonState}
       </button>
     </form>
+  );
+};
+
+interface AmountInputProps {
+  inputName?: string;
+  amount: string;
+  setAmount: (amount: string) => void;
+  currency: Token;
+  setCurrency: (currency: Token) => void;
+  showCurrencyDropdown: boolean;
+  setShowCurrencyDropdown: (show: boolean) => void;
+  strategy: InvestStrategy;
+  isLoadingBalance: boolean;
+  isSupportedChain: boolean;
+  maxBalance: number;
+  handleSetMax: () => void;
+}
+
+const AmountInput = ({
+  inputName = "amount",
+  amount,
+  setAmount,
+  currency,
+  setCurrency,
+  showCurrencyDropdown,
+  setShowCurrencyDropdown,
+  strategy,
+  isLoadingBalance,
+  isSupportedChain,
+  maxBalance,
+  handleSetMax,
+}: AmountInputProps) => {
+  return (
+    <div className="bg-gray-100 rounded-md border border-gray-300 mb-6">
+      <div className="flex items-center w-full gap-2">
+        <input
+          type="text"
+          name={inputName}
+          id={inputName}
+          className="flex-1 min-w-0 bg-transparent text-gray-500 block px-4 py-3 text-lg font-semibold focus:outline-none focus:ring-0 focus:border-0 placeholder:text-gray-500"
+          placeholder="0.00"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        {/* Custom dropdown with icons */}
+        <div className="shrink-0 md:min-w-[100px] relative">
+          <button
+            type="button"
+            className="text-sm md:text-lg bg-transparent flex items-center gap-1 md:gap-2 px-2 md:px-4 py-2 font-semibold focus:outline-none rounded-md hover:bg-gray-200"
+            onClick={() => setShowCurrencyDropdown(!showCurrencyDropdown)}
+          >
+            <Image
+              src={currency.icon}
+              alt={currency.name}
+              className="w-6 h-6 object-contain"
+              width={24}
+              height={24}
+            />
+            {currency.name}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3 md:h-5 md:w-5 md:ml-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </button>
+          {showCurrencyDropdown && (
+            <div className="absolute right-0 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10">
+              {strategy.tokens.map((token) => (
+                <button
+                  key={token.name}
+                  type="button"
+                  className="w-full flex items-center gap-1 px-2 md:gap-2 md:px-4 py-2 text-left hover:bg-gray-100"
+                  onClick={() => {
+                    setCurrency(token);
+                    setShowCurrencyDropdown(false);
+                  }}
+                >
+                  <Image
+                    src={token.icon}
+                    alt={token.name}
+                    width={24}
+                    height={24}
+                    className="w-6 h-6 object-contain"
+                  />
+                  {token.name}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="flex items-center px-4 pb-2">
+        <div className="flex items-center w-full">
+          <span className="flex items-center gap-2 text-xs md:text-sm text-gray-500">
+            <span>Balance: </span>
+            <div>
+              {isLoadingBalance ? (
+                <MoonLoader size={10} />
+              ) : isSupportedChain ? (
+                maxBalance.toFixed(4)
+              ) : (
+                "NaN"
+              )}
+            </div>
+            <span>{currency.name}</span>
+          </span>
+          <button
+            type="button"
+            onClick={handleSetMax}
+            disabled={!isSupportedChain || isLoadingBalance}
+            className="text-xs md:text-sm font-medium text-[#5F79F1] hover:text-[#4A64DC] focus:outline-none ml-2 border-0 bg-transparent cursor-pointer disabled:opacity-50"
+          >
+            MAX
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
